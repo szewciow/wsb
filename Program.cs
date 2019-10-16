c3689fd5eb1defcfa513b15dd486a590a0c8dcb7
szewciow <56116050+szewciow@users.noreply.github.com>
2019-10-16 23:55:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace gwizd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string z;
+            z = Console.ReadLine();
+            int w;
+            if (int.TryParse(z, out w))
+            {
+                for (int a = 1; a <= w; a++)
+                {
+                    for (int b = 0; b < a; b++)
+                        Console.Write("*");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
